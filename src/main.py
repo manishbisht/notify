c19ae0cc4fd3539771b3c5b42eb2988225d2ea1a
Manish Bisht <manish.bisht490@gmail.com>
2017-02-27 19:20:16 +0530

    getCurrrentHackerrankContest added

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import urllib, json, time, urllib2, datetime
 import bs4
 
+
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -66,12 +67,14 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+
 def getNextCodechefContest(intent, session):
     """ Get the next codechef contest details and prepares the speech to reply to the user.
     """
     card_title = "Codechef Next Contest Details"
     session_attributes = {}
     should_end_session = True
+    reprompt_text = ""
     codechef_contest_link = "https://www.codechef.com/contests"
     page = urllib2.urlopen(codechef_contest_link)
     soup = bs4.BeautifulSoup(page, "html.parser")
@@ -99,12 +102,13 @@ def getNextCodechefContest(intent, session):
         m = divmod(h[1], 60)
         s = m[1]
         speech_output = "The next contest " + contest_name + " on codechef will start in " \
-                        '%d days, %d hours, %d minutes, %d seconds' % (d[0], h[0], m[0], s)
+                                                             '%d days, %d hours, %d minutes, %d seconds' % (
+                                                                 d[0], h[0], m[0], s)
     else:
         speech_output = "There are no upcoming contest on codechef."
-    reprompt_text = ""
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
 
 def getCurrrentCodechefContest(intent, session):
     """ Get the current codechef contest details and prepares the speech to reply to the user.
@@ -112,6 +116,7 @@ def getCurrrentCodechefContest(intent, session):
     card_title = "Codechef Current Contest Details"
     session_attributes = {}
     should_end_session = True
+    reprompt_text = ""
     codechef_contest_link = "https://www.codechef.com/contests"
     page = urllib2.urlopen(codechef_contest_link)
     soup = bs4.BeautifulSoup(page, "html.parser")
@@ -134,11 +139,57 @@ def getCurrrentCodechefContest(intent, session):
         m = divmod(h[1], 60)
         s = m[1]
         speech_output = "The contest " + contest_name + " on codechef is running. It will end in " \
-                        '%d days, %d hours, %d minutes, %d seconds' % (d[0], h[0], m[0], s)
+                                                        '%d days, %d hours, %d minutes, %d seconds' % (
+                                                            d[0], h[0], m[0], s)
     else:
         speech_output = "There is no contest running on codechef."
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
 
+
+def getCurrrentHackerrankContest(intent, session):
+    """ Get the current hackerrank contest details and prepares the speech to reply to the user.
+    """
+    card_title = "Hackerrank Current Contest Details"
+    session_attributes = {}
+    should_end_session = True
     reprompt_text = ""
+    hackerrank_contest_link = "https://www.hackerrank.com/contests"
+    page = urllib2.urlopen(hackerrank_contest_link)
+    soup = bs4.BeautifulSoup(page, "html.parser")
+    list = soup.find_all("div", class_="active_contests")
+    speech_output = "There is no contest running on hackerrank."
+    if len(list) > 0:
+        list = list[0].find_all('li')
+        list.pop(0)
+        if len(list) > 0:
+            contest = []
+            for r in list:
+                temp = r.find_all('button')
+                if len(temp) > 0 and temp[0].text == "Sign Up":
+                    contest = r
+                    break
+            contest_name = contest.find_all('span')[0].text
+            start = contest.find_all('meta')[0]["content"]
+            start = time.strptime(start[0:19], '%Y-%m-%dT%H:%M:%S')
+            start = time.mktime(start)
+            end = contest.find_all('meta')[1]["content"]
+            end = time.strptime(end[0:19], '%Y-%m-%dT%H:%M:%S')
+            end = time.mktime(end)
+            then = int(time.time())
+            if then < start:
+                now = start
+            else:
+                now = end
+            d = divmod(now - then, 86400)
+            h = divmod(d[1], 3600)
+            m = divmod(h[1], 60)
+            s = m[1]
+
+            if then > start:
+                speech_output = "The contest " + contest_name + " on hackerrank is running and it will end in " \
+                                                                '%d days, %d hours, %d minutes, %d seconds' % (
+                                                                    d[0], h[0], m[0], s)
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -183,15 +234,18 @@ def getHackerrankContest(intent, session):
 
             if then < start:
                 speech_output = "The next contest " + contest_name + " on codeforces will start in next " \
-                        '%d days, %d hours, %d minutes, %d seconds' % (d[0], h[0], m[0], s)
+                                                                     '%d days, %d hours, %d minutes, %d seconds' % (
+                                                                         d[0], h[0], m[0], s)
             else:
                 speech_output = "The contest " + contest_name + " on Hackerrank is running and it will end in " \
-                        '%d days, %d hours, %d minutes, %d seconds' % (d[0], h[0], m[0], s)
+                                                                '%d days, %d hours, %d minutes, %d seconds' % (
+                                                                    d[0], h[0], m[0], s)
 
     reprompt_text = "Please tell me from where you want the contest details by saying, " \
                     "When is the codeforces next contest."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
+
 
 def getCodeforcesContest(intent, session):
     """ Get the codeforces contest details and prepares the speech to reply to the user.
@@ -219,7 +273,8 @@ def getCodeforcesContest(intent, session):
             m = divmod(h[1], 60)
             s = m[1]
             speech_output = "The next contest " + result["name"] + " will start in " \
-                            '%d days, %d hours, %d minutes, %d seconds' % (d[0], h[0], m[0], s)
+                                                                   '%d days, %d hours, %d minutes, %d seconds' % (
+                                                                       d[0], h[0], m[0], s)
     else:
         speech_output = "Sorry, Right now we are facing technical issues. " \
                         "Please try again after some time."
@@ -265,7 +320,7 @@ def on_intent(intent_request, session):
     if intent_name == "NextContestIntent" and website.lower() == "codechef":
         return getNextCodechefContest(intent, session)
     elif intent_name == "CurrentContestIntent" and website.lower() == "codechef":
-        return getCurrentCodechefContest(intent, session)
+        return getCurrrentCodechefContest(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
